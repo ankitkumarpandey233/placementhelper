2ffa07c64cfede64b96606f128607e90d3fbe4de
Ankit <ankitpandey21@gnu.ac.in>
2023-11-08 10:42:05 +0530

    removing everything

--- Sql/rejected.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `rejected` (
-  `studentEmail` varchar(100) DEFAULT NULL,
-  `studentName` varchar(100) DEFAULT NULL,
-  `companyEmail` varchar(100) DEFAULT NULL,
-  `companyName` varchar(100) DEFAULT NULL,
-  `studentEnrollment` varchar(100) DEFAULT NULL,
-  `round` int(11) NOT NULL DEFAULT 0
-)
